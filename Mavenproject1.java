f07f7fd1eaba2f9325d01aaa23d164023f913fec
NechitaAlexandru <nechita.alex6@yahoo.com>
2026-02-20 11:01:12 +0200

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,34 @@
+package com.mycompany.mavenproject1;
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        //1
+        System.out.println("Hello World!");
+        //2 
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        
+        //3
+        int n = (int) (Math.random() * 1_000_000);
+        
+        //4
+        int result = n*3;
+        result += 0b10101;
+        result += 0xFF;
+        result *= 6;
+        //5
+        
+        while (result>9)
+        {
+            int sum=0;
+            while(result>0) {
+                sum+=result%10;
+                result /=10;
+            }
+            result=sum;
+        }
+        //6
+        System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
+    }
+    
+}
